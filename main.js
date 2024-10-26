4e8176e67f51da0ad9a97aaad93d44eee5b0aeed
Naima <Naima.IbrahimAbdirahman@metropolia.fi>
2024-10-26 14:57:15 +0300

    Implement main application logic

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+const navList = document.querySelector(".nav-list");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+function toggleMenu() {
+    navList.classList.toggle("expanded");
+
+    // Change button text based on menu state and different icon state
+    if (navList.classList.contains("expanded")) {
+        hamburgerMenu.textContent = "X";
+    } else {
+        hamburgerMenu.textContent = "☰"; 
+    }
+}
