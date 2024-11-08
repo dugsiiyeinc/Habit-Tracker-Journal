7c1877ba423b4bfd0000e0b1eba39b561ba5b2c2
caaaqil <cabduqaadirshariif68@gmail.com>
2024-11-08 23:18:25 +0300

    added contact page with style

---
+++ contact.js
@@ -0,0 +1,4 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for you contact we'll reply as soon as possible");
+});
