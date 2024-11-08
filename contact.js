e6fb1e0bc809aa65e24682f21ec28cf2bb1550ca
caaaqil <cabduqaadirshariif68@gmail.com>
2024-11-09 00:03:48 +0300

    change contact page style

--- contact.js
+++ contact.js
@@ -1,4 +1,6 @@
+// JavaScript to handle form submission
 document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
     alert("Thank you for you contact we'll reply as soon as possible");
+    document.querySelector("form").reset();
 });
